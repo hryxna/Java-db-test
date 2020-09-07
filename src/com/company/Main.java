2e92478e704a265a45302dc085dd361aac7a018a
hryxna <hk20726@gmail.com>
2020-09-07 12:11:14 +0530

    basic structure of sql

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.sql.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        String url = "";
+        String user = "root";
+        String passwd = "root@123";
+
+        try {
+            Connection con = DriverManager.getConnection(url,user,passwd);
+            Statement st = con.createStatement();
+            String sqlq = ""; // sql query
+            ResultSet rs = st.executeQuery(sqlq);
+
+            while(rs.next()){
+                // any sql thing you want to retrieve
+            }
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 }
